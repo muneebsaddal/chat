d7e8c932a71065004aef3823d60813f0c03c9ea1
Muneeb Saddal <muneebsaddal@outlook.com>
2022-06-23 13:12:13 +0500

    random email generation for testing

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,3 +1,6 @@
+const num = Math.floor(Math.random() * 100 + 1);
+const email = "test-".concat(num).concat("@gmail.com");
+
 /* eslint-disable no-undef */
 describe("Login, Register and Authentication", () => {
 	it("Register User", () => {
@@ -5,7 +8,7 @@ describe("Login, Register and Authentication", () => {
 			headers: { "Accept-Encoding": "gzip, deflate" },
 		});
 		cy.get("input#name").type("test");
-		cy.get("input#email").type("test5@gmail.com");
+		cy.get("input#email").type(email);
 		cy.get("input#password").type("qwerty");
 		cy.get("input#confirm-password").type("qwerty");
 		cy.get('button[id="register-button"]').click();
@@ -13,7 +16,7 @@ describe("Login, Register and Authentication", () => {
 	});
 
 	it("Login User", () => {
-		cy.get("input#email").type("test5@gmail.com");
+		cy.get("input#email").type(email);
 		cy.get("input#password").type("qwerty");
 		cy.get('button[id="login-button"]').click();
 		cy.get('button[id="logout"]').click();
